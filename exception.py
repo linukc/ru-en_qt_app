90405aa7996be167de77b93ecae528fa6a74c0db
Sergei Linok <serega.linok5555@yandex.ru>
2021-03-08 16:57:36 +0300

    Add login window

--- exception.py
+++ exception.py
@@ -1,21 +1,35 @@
 class MainWindow_BaseError(Exception):
     pass
 
-class LoginWindow_BaseError(Exception):
-    pass
-
-class TestWindow_BaseError(Exception):
-    pass
-
 class Wrong_Search_Language(MainWindow_BaseError):
-    #error_msg = 'Поменяйте язык для ввода или нажмите на кнопку смены таблиц'
-    error_title = "Cannot recognize searching language"
-    error_msg = "Switch lang or \n press switch tables button"
+    error_msg = 'Поменяйте язык для ввода или нажмите на кнопку смены таблиц'
+    #error_title = "Cannot recognize searching language"
+    #error_msg = "Switch lang or \n press switch tables button"
 
 class Wrong_Translation_Language(MainWindow_BaseError):
-    error_title = "Cannot recognize translation language"
+    #error_title = "Cannot recognize translation language"
     error_msg = "Switch lang or \n press switch tables button"
 
 class EmptyLine(MainWindow_BaseError):
-    error_title = "Note enough"
+    #error_title = "Note enough"
     error_msg = "One of the stringboxes is empty"
+
+
+class LoginWindow_BaseError(Exception):
+    pass
+
+class LoginForbiddenSymbols(LoginWindow_BaseError):
+    error_title = "forbidden symbols in login"
+    error_msg = "only eng symbols with numbers" 
+
+class PasswordForbiddenSymbols(LoginWindow_BaseError):
+    error_title = "forbidden symbols in password"
+    error_msg = "only eng symbols with numbers" 
+
+class EmptyLine_LW(LoginWindow_BaseError):
+    error_title = 'Warning'
+    error_msg = "One of the stringboxes is empty"
+
+
+class TestWindow_BaseError(Exception):
+    pass
--- main.py
+++ main.py
@@ -1,30 +1,65 @@
 import sys
 import exception as e
 import database
-from string import ascii_letters as en_letters
+from string import ascii_letters, digits
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QWidget
 from ui_main import Ui_MainWindow
+from ui_login import Ui_Form as Ui_Login
 
 
-class MainWindow(QMainWindow, Ui_MainWindow):
+class LoginWindow(QWidget, Ui_Login):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.lang = {'ru': 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ', 'en': en_letters}
+        self.allowed_symbols = ascii_letters + digits
+        self.SignInButton.clicked.connect(self.SignIn)
+        self.SignUpButton.clicked.connect(self.SignUp)
+
+    def SignBase(self):
+        login = self.LoginLine.text()
+        password = self.PasswordLine.text()
+        if not login or not password:
+            raise e.EmptyLine_LW()
+        elif not alphabet_text(login, self.allowed_symbols):
+            raise e.LoginForbiddenSymbols()
+        elif not alphabet_text(password, self.allowed_symbols):
+            raise e.PasswordForbiddenSymbols()
+
+    def SignIn(self):
+        self.SignBase()
+        self.open_MainWindow()
+
+    def SignUp(self):
+        self.SignBase()
+
+    def open_MainWindow(self):
+        self.MainWindow = MainWindow(self)
+        self.hide()
+        self.MainWindow.show()
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args):
+        super().__init__()
+        self.setupUi(self)
+        self.lang = {'ru': 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ', 'en': ascii_letters}
         self.first_lang = 'ru'
         self.second_lang = 'en'
         self.AddTranslationButton.clicked.connect(self.EnableAddingTranslation)
         self.CancelAddingTranslationButton.clicked.connect(self.CancelTranslationAdding)
         self.SubmitTranslationButton.clicked.connect(self.AddTranslation)
+        self.SwapTablesButton.clicked.connect(self.SwapTables)
+        self.SearchLine.textChanged.connect(self.SearchWord)
         #создать таблицу после логина юзера (т к от его название его таблицы = логин + пароль)
         #db = database.MainWindowDataBase(self)
+        #добавить кнопку выхода
 
     def EnableAddingTranslation(self):
         if not self.SearchLine.text():
             raise e.EmptyLine()
-        elif not all([symb in self.lang.get(self.first_lang) for symb in self.SearchLine.text()]):
+        elif not alphabet_text(self.SearchLine.text(), self.lang.get(self.first_lang)):
             raise e.Wrong_Search_Language()
         else:
             self.TranslationLine.setEnabled(True)
@@ -34,10 +69,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def AddTranslation(self):
         if not self.SearchLine.text() or not self.TranslationLine.text():
             raise e.EmptyLine()
-        elif not all([symb in self.lang.get(self.first_lang) for symb in self.SearchLine.text()]):
+        elif not alphabet_text(self.SearchLine.text(), self.lang.get(self.first_lang)):
             raise e.Wrong_Search_Language()
-        elif not all([symb in self.lang.get(self.second_lang) for symb in self.TranslationLine.text()]):
-            raise e.Wrong_Translation_Language
+        elif not alphabet_text(self.TranslationLine.text(), self.lang.get(self.second_lang)):
+            raise e.Wrong_Translation_Language()
         else:
             self.CancelTranslationAdding()
 
@@ -50,14 +85,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         
     def SwapTables(self):
         self.first_lang, self.second_lang = self.second_lang, self.first_lang
-        
+
+    def SearchWord(self):
+        word = self.SearchLine.text()
+        #print(word)
+
+
+def alphabet_text(text, alphabet):
+    return all([symb in alphabet for symb in text])
+
 
 def except_hook(cls, exception, traceback):
     if e.MainWindow_BaseError in cls.__bases__:
-        QMessageBox.critical(None, cls.error_title, cls.error_msg, QMessageBox.Cancel)
-        #main_window.statusBar().showMessage(cls.error_msg, 5000)
+        login_window.MainWindow.statusBar().showMessage(cls.error_msg, 5000)
     elif e.LoginWindow_BaseError in cls.__bases__:
-        pass
+        QMessageBox.critical(None, cls.error_title, cls.error_msg, QMessageBox.Cancel)
     elif e.TestWindow_BaseError in cls.__bases__:
         pass
     else:
@@ -66,7 +108,9 @@ def except_hook(cls, exception, traceback):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_window = MainWindow()
-    main_window.show()
+    
+    login_window = LoginWindow()
+    login_window.show()
+
     sys.excepthook = except_hook
     sys.exit(app.exec_())
